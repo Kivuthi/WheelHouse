f299e2697f33989db63717f7ff9fb0b323110ab0
West Side <officialdee77@gmail.com>
2025-09-10 09:32:47 +0300

    initial commit

---
+++ dashboards/index.php
@@ -0,0 +1,73 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WheelHouse</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="headaer">
+
+        <div class="logo">
+            <img src="" alt="">
+        </div>
+
+        <div class="links">
+            <a href="#">Cars</a>
+            <a href="#">Bikes</a>
+            <a href="#">Import</a>
+            <a href="#">Dealers</a>
+        </div>
+
+        <div class="buttons">
+            <button type="submit">Login</button>
+            <button type="submit">Become a Seller</button>
+        </div>
+        </div>
+    </div>
+
+     <div class="container2">
+        <div class="hero-section">
+            <p><h1>West-Side Co.</h1></p>
+            <p><h3>Your Premier Automotive Marketplace</h3></p>
+            <p>Discover premium vehicles from trusted dealers. Cars, bikes, and import vehicles all in one place.</p>
+            <div class="quick-link">
+                <button type="submit">Browse Vehicles -></button>
+                <button type="submit">Become a Seller</button>
+            </div>
+        </div>
+     </div>
+
+     <div class="container3">
+        <div class="search">
+            <input type="text" name="search" placeholder="Search by brand, model, or keyword...">
+            <button type="submit">All Vehicles</button>
+            <button type="submit">cars</button>
+            <button type="submit">Bikes</button>
+            <button type="submit">Search</button>
+            <button type="submit">filter</button>
+        </div>
+     </div>
+
+     <div class="container4">
+        <h4>Popular: </h4>
+        <button type="button">Luxury cars</button>
+        <button type="button">SUVs</button>
+        <button type="button">Electric</button>
+        <button type="button">Under $20k</button>
+        <button type="button">New Arrivals</button>
+        <button type="button">Sport Bikes</button>
+     </div>
+
+
+
+    <!-- #414A37, #99744A, #DBC2A6 -->
+    <!-- #50207A, #D6B9FC, #838CE5 -->
+</body>
+</html>
---
+++ sql/database.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    company VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    role ENUM('seller', 'buyer', 'admin') DEFAULT 'buyer',
+    status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    
+ CREATE TABLE cars (
+     ID INT AUTO_INCREMENT PRIMARY KEY,
+     seller_id INT,
+     title VARCHAR(100),
+     yom INT,
+     transmission ENUM('manual', 'automatic', 'automated manual', 'continuously variable transmission') DEFAULT 'automatic',
+     mileage VARCHAR(50),
+     location VARCHAR(50),
+     engine_size VARCHAR(50),
+     drive ENUM('2WD', '4WD', 'AWD'),
+     fuel ENUM('petrol', 'diesel', 'hybrid', 'electric'),
+     description TEXT,
+     price DECIMAL(10,2),
+     image VARCHAR(255),
+     category ENUM('car','bike') DEFAULT 'car',
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     FOREIGN KEY (seller_id) REFERENCES users(id)
+    );
+    
