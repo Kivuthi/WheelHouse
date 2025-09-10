8934d23f57c5a9789cb76bc184d29e0b2a57b463
West Side <officialdee77@gmail.com>
2025-09-10 11:29:21 +0300

    update

--- dashboards/index.php
+++ dashboards/index.php
@@ -51,9 +51,50 @@
             <button type="submit">cars</button>
             <button type="submit">Bikes</button>
             <button type="submit">Search</button>
-            <button type="submit">filter</button>
+            <button class="filter" type="button">filter</button>
         </div>
      </div>
+
+     <div class="container3B">
+        <div class="accordion">
+            <div class="accordion-content">
+                <div class="budget-filter">
+                    <label for="brand">Brand</label>
+                    <select name="brand" id="brand"></select>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+
+                    <label for="budget">Budget</label>
+                    <select name="price" id="price"></select>
+                    <option value="under $10,000">Under $10,000</option>
+                    <option value="$10,000-$25,000">$10,000-$25,000</option>
+                    <option value="$25,000-$50,000">$25,000-$50,000</option>
+                    <option value="$50,000-$75,000">$50,000-$75,000</option>
+                    <option value="$75,000-$100,000">$75,000-$100,000</option>
+                    <option value="above $100,000">Above $100,000</option>
+
+                    <label for="year">Year</label>
+                    <input type="year" name="year" placeholder="From">
+                    <input type="year" name="year" placeholder="To">
+
+                    <label for="location">Location</label>
+                    <input type="text" name="loaction" placeholder="city or ZIP code">
+                </div>
+            </div>
+            </div>
+        </div>
 
      <div class="container4">
         <h4>Popular: </h4>
@@ -66,7 +107,22 @@
      </div>
 
 
+    <script>
+  const headers = document.querySelectorAll(".filter");
 
+  headers.forEach(header => {
+    header.addEventListener("click", () => {
+      const content = header.nextElementSibling;
+      content.classList.toggle("open");
+
+      if (content.classList.contains("open")) {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } else {
+        content.style.maxHeight = 0;
+      }
+    });
+  });
+</script>
     <!-- #414A37, #99744A, #DBC2A6 -->
     <!-- #50207A, #D6B9FC, #838CE5 -->
 </body>
